c877e56b43e68979b9ebe4bcacf149bde5a9f6bb
RobertSantos7 <robert@rfadatainsights.com>
2025-09-22 22:43:01 -0700

    Update next-intl.config.ts

--- next-intl.config.ts
+++ next-intl.config.ts
@@ -1,9 +1,10 @@
+// next-intl.config.ts
 import { IntlConfig } from 'next-intl';
 
 const nextIntlConfig: IntlConfig = {
-  locales: ['en', 'es'],
-  defaultLocale: 'en',
-  messagesDirectory: './messages'
+  locales: ['en', 'es'],          // Idiomas soportados
+  defaultLocale: 'en',             // Idioma por defecto
+  messagesDirectory: './messages'  // Carpeta de traducciones
 };
 
 export default nextIntlConfig;
