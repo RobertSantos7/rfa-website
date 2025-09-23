9fb3ac346993b293ccff5aeb2dc847f679e00069
RobertSantos7 <robert@rfadatainsights.com>
2025-09-22 23:03:37 -0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,10 @@
 // middleware.ts
 import createMiddleware from 'next-intl/middleware';
 
-export default createMiddleware();
+export createMiddleware({
+  locales: ['en-US'], // An array of supported locales
+  defaultLocale: 'en-US', // The default locale
+});
 
 export const config = {
   matcher: ['/((?!api|_next|.*\\..*).*)'], // todas las rutas excepto /api y assets
