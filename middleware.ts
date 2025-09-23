b91637b755adfc0fd26260b8a76bf23dac88e1f6
RobertSantos7 <robert@rfadatainsights.com>
2025-09-22 22:22:09 -0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,10 @@
-import createMiddleware from "next-intl/middleware";
+import createMiddleware from 'next-intl/middleware';
 
 export default createMiddleware({
-  locales: ["en", "es"],   // idiomas disponibles
-  defaultLocale: "en",     // por defecto: inglés
-  localeDetection: true    // detecta idioma del navegador y redirige a /es o /en
+  locales: ['en', 'es'],
+  defaultLocale: 'en'
 });
 
 export const config = {
-  matcher: ["/((?!_next|.*\\..*).*)"] // ignora assets y _next
+  matcher: ['/((?!api|_next|.*\\..*).*)']
 };
