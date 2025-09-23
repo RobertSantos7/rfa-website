c2ca0c58c5781d6910039a9aa700f70fc2e85e4d
RobertSantos7 <robert@rfadatainsights.com>
2025-09-22 22:12:09 -0700

    Create next-intl.config.ts

---
+++ next-intl.config.ts
@@ -0,0 +1,9 @@
+// next-intl.config.ts
+import { NextIntlConfig } from 'next-intl';
+
+const nextIntlConfig: NextIntlConfig = {
+  locales: ['en', 'es'], // idiomas soportados
+  defaultLocale: 'en'    // idioma por defecto
+};
+
+export default nextIntlConfig;
